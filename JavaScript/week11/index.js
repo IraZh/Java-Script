b4584a24e7c38161cfc2fccb469615974cce5147
Ira Zh <irishazhgun@gmail.com>
2023-01-30 04:14:41 +0300

    add

---
+++ JavaScript/week11/index.js
@@ -0,0 +1,24 @@
+let price1=document.getElementById('price_1').innerHTML;
+let price_1=+price1;
+console.log(price_1);
+
+let price2=document.getElementById('price_2').innerHTML;
+let price_2=+price2;
+console.log(price_1);
+
+let price3=document.getElementById('price_3').innerHTML;
+let price_3=+price3;
+console.log(price_3);
+
+let price4=document.getElementById('price_4').innerHTML;
+let price_4=+price4;
+console.log(price_4);
+
+let submit=price_1+price_2+price_3+price_4;
+console.log(submit);
+
+
+
+function discount(){
+  
+};
