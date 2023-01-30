6ed4c31c21561fbe47cf9d53e2158c226a842cb0
Ira Zh <irishazhgun@gmail.com>
2023-01-30 20:53:19 +0300

    add function

--- JavaScript/week11/index.js
+++ JavaScript/week11/index.js
@@ -17,8 +17,15 @@ console.log(price_4);
 let submit=price_1+price_2+price_3+price_4;
 console.log(submit);
 
-
-
 function discount(){
-  
+    const discount = submit - submit*0.2;
+    document.querySelector("#submit").textContent = discount;
+    const discount1=price_1-price_1*0.2;
+    document.querySelector("#price_1").textContent = discount1;
+    const discount2=price_2-price_2*0.2;
+    document.querySelector("#price_2").textContent = discount2;
+    const discount3=price_3-price_3*0.2;
+    document.querySelector("#price_3").textContent = discount3;
+    const discount4=price_4-price_4*0.2;
+    document.querySelector("#price_4").textContent = discount4;
 };
