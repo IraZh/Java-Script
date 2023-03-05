c14250d7b1bed976b5bdc88073876ea3ec09b881
Ira Zh <irishazhgun@gmail.com>
2023-03-05 03:03:07 +0300

    week 17

--- JavaScript/week17/part1/index.js
+++ JavaScript/week17/part1/index.js
@@ -7,14 +7,7 @@ class Worker {
     }
 
     getSalary(){
-        return salary = this.rate * this.days;
+        const salary = this.rate * this.days;
+        return salary;
     };
 }
-
-var worker = new Worker('Иван', 'Иванов', 10, 31);
-
-console.log(worker.name); //выведет 'Иван'
-console.log(worker.surname); //выведет 'Иванов'
-console.log(worker.rate); //выведет 10
-console.log(worker.days); //выведет 31
-console.log(worker.getSalary()); //выведет 310 — то есть 10*31
